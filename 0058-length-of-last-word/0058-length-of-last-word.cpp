132b25f50699c25dc987fa841ffeeff4f843d2e0
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2023-07-27 02:03:25 +0530

    Time: 5 ms (39.19%), Space: 6.5 MB (84.34%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int cnt=0;
-        for(int i=s.length()-1;i>=0;i--)
+        for(int i=s.size()-1;i>=0;i--)
         {
             if(isspace(s[i])&&cnt!=0) break;
             if(!isspace(s[i])) cnt++;
