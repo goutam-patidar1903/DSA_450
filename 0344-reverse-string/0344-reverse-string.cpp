45eb88a3e439a665686270b327b5b4f3d0f9799a
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2023-08-05 23:58:04 +0530

    Time: 21 ms (70.27%), Space: 23.2 MB (18.51%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -3,14 +3,9 @@ public:
     void reverseString(vector<char>& s) {
         int i=0;
         int j=s.size()-1;
-        char c;
         while(i<j)
         {
-            c=s[i];
-            s[i]=s[j];
-            s[j]=c;
-            i++;
-            j--;
+           swap(s[i++],s[j--]); 
         }
     }
 };
