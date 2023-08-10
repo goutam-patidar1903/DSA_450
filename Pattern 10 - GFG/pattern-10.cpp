30d557605508115441131254bb0b571ce5004b3f
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2023-08-11 00:17:30 +0530

    Added solution - LeetHub

---
+++ Pattern 10 - GFG/pattern-10.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+class Solution{
+public:
+    void printTriangle(int n) {
+        // code here
+        int star=1;
+        for(int i=1;i<(2*n);i++)
+        {
+            for(int j=1;j<=star;j++) cout<<"* ";
+            cout<<endl;
+            if(i<n) star++;
+            else star--;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
