a4aa1d70a720e727ad2e873a4e569e04b836e03b
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2023-08-17 22:12:58 +0530

    Time: 10 ms (95.13%), Space: 19.6 MB (59.22%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -7,20 +7,20 @@ public:
         {
             
            m[nums[i]]++; 
-            auto it = m.find(nums[i]);
-            if(it != m.end() && it->second > n/2) return nums[i];
+            // auto it = m.find(nums[i]);
+            // if(it != m.end() && it->second > n/2) return nums[i];
         }
         int maxElem,maxCount=0;
-        // for(auto it:m)
-        // {
-        //     if(it.second > maxCount)
-        //     {
-        //         maxCount = it.second;
-        //         maxElem = it.first;
-        //     }
-        // }
+        for(auto it:m)
+        {
+            if(it.second > maxCount)
+            {
+                maxCount = it.second;
+                maxElem = it.first;
+            }
+        }
             
         
-        return 0;
+        return maxElem;
     }
 };
