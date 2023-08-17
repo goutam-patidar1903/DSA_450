de28b16cd2da800ab990fff143c86a4c5340344f
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2023-08-17 22:12:20 +0530

    Time: 22 ms (41.01%), Space: 19.6 MB (59.22%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int ,int> m;
-        for(int i=0;i<nums.size();i++)
+        int n=nums.size();
+        for(int i=0;i<n;i++)
         {
+            
            m[nums[i]]++; 
+            auto it = m.find(nums[i]);
+            if(it != m.end() && it->second > n/2) return nums[i];
         }
         int maxElem,maxCount=0;
-        for(auto it:m)
-        {
-            if(it.second > maxCount)
-            {
-                maxCount = it.second;
-                maxElem = it.first;
-            }
-        }
+        // for(auto it:m)
+        // {
+        //     if(it.second > maxCount)
+        //     {
+        //         maxCount = it.second;
+        //         maxElem = it.first;
+        //     }
+        // }
             
         
-        return maxElem;
+        return 0;
     }
 };
