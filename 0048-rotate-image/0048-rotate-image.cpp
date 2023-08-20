d0166b7ee44400de412cdf3dd14dab8184e4d05f
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2023-08-20 21:46:40 +0530

    Time: 4 ms (35.66%), Space: 7 MB (49.75%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        queue<int> q;
         int n = matrix.size();
-        for(int i=0;i<n;i++)
+        // queue<int> q;
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         q.push(matrix[i][j]);       
+        //     }
+        // }
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         matrix[j][n-i-1]=q.front();
+        //         q.pop();
+        //     }
+        // }
+        for(int i=0;i<n-1;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=i+1;j<n;j++)
             {
-                q.push(matrix[i][j]);       
+                swap(matrix[i][j] , matrix[j][i]);
             }
         }
-        for(int i=0;i<n;i++)
+        for(int i=0; i<n ; i++)
         {
-            for(int j=0;j<n;j++)
+            int p=0,q=n-1;
+            while(p<q)
             {
-                matrix[j][n-i-1]=q.front();
-                q.pop();
+                swap(matrix[i][p] , matrix[i][q]);
+                p++;
+                q--;
             }
         }
     }
