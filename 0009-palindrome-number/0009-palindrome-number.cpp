7b34038b9bcff5dc7963f725472665e88c8e4cc5
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2024-05-20 08:54:43 +0530

    Time: 3 ms (93.47%), Space: 8.3 MB (48.32%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int size;
-        char s[51];
-        sprintf(s,"%d",x);
-        size=strlen(s);
-        int j=size-1;
-        int i;
-        for(i=0;i<(size/2);i++)
-        {
-            if(s[i]!=s[j]) break;
-            j--;
+         if (x < 0) {
+            return false;
         }
-        if(j<=i) return true;
-        return false;
+
+        long long reversed = 0;
+        long long temp = x;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return (reversed == x);
     }
 };
