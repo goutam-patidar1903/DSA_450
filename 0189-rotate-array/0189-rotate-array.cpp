e784336328ecc5720424d57ba1fd5b1a36efa648
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2024-05-24 11:56:42 +0530

    Time: 21 ms (54.94%), Space: 28.1 MB (7.76%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,55 +1,38 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+//         int size=nums.size();
+//         if(size==1) return;
+//         k=k%size;
+//         vector<int> temp;
+//         int x=size-k;
+//         for(int i=0;i<size;i++)
+//         {
+//             temp.push_back(nums[(x+i)%size]);
+//         }
+//         for(int i=0;i<size;i++)
+//         {
+//             nums[i]=temp[i];
+//         }
+        
+        
         int size=nums.size();
         if(size==1) return;
         k=k%size;
-        vector<int> temp;
-        int x=size-k;
-        for(int i=0;i<size;i++)
+        vector<int> tmp;
+        for(int i=1;i<=k;i++)
         {
-            temp.push_back(nums[(x+i)%size]);
+            tmp.push_back(nums[size-i]);
         }
-        for(int i=0;i<size;i++)
+        for(int i=size-k-1;i>=0;i--)
         {
-            nums[i]=temp[i];
+            nums[i+k]=nums[i];
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // int size=nums.size();
-        // if(size==1) return;
-        // k=k%size;
-        // vector<int> tmp;
-        // for(int i=1;i<=k;i++)
-        // {
-        //     tmp.push_back(nums[size-i]);
-        // }
-        // for(int i=size-k-1;i>=0;i--)
-        // {
-        //     nums[i+k]=nums[i];
-        // }
-        // int j=k-1;
-        // for(int i=0;i<tmp.size();i++)
-        // {
-        //     nums[j]=tmp[i];
-        //     j--;
-        // }
+        int j=k-1;
+        for(int i=0;i<tmp.size();i++)
+        {
+            nums[j]=tmp[i];
+            j--;
+        }
     }
 };
