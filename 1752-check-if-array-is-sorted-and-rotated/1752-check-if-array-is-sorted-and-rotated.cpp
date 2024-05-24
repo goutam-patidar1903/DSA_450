079107adba94e9f3dba5c730c1af3b4ec229c72e
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2024-05-24 10:55:23 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (61.18%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,20 +1,59 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int size=nums.size();
-        for(int i=0;i<(size-1);i++)
-        {   
-            if(nums[i]>nums[i+1]) 
+       int size=nums.size();
+        if(size<=1) return true;
+        int smallest=nums[0];
+        int x=0;
+        for(int i=1;i<size;i++)
+        {
+            if(nums[i]<=smallest && nums[i]!=nums[i-1])
             {
-                i++;
-                for(int j=0;j<(size-1);j++)
-                {
-                    if(nums[i]>nums[(i+1)%size]) { return false; }
-                    i=(i+1)%size;
-                }
-                return true;
+                smallest=nums[i];
+                x=i;
             }
         }
+        for(int i=0;i<(size-1);i++)
+        {
+            int p=(x+i)%size;
+            int q=(x+i+1)%size;
+            if(nums[p]>nums[q]) return false;
+        }
         return true;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // int size=nums.size();
+        // for(int i=0;i<(size-1);i++)
+        // {   
+        //     if(nums[i]>nums[i+1]) 
+        //     {
+        //         i++;
+        //         for(int j=0;j<(size-1);j++)
+        //         {
+        //             if(nums[i]>nums[(i+1)%size]) { return false; }
+        //             i=(i+1)%size;
+        //         }
+        //         return true;
+        //     }
+        // }
+        // return true;
     }
 };
