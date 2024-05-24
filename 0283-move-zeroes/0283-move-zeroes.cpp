05515ef51007047ba073ccbc093b7235872aca69
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2024-05-24 12:02:08 +0530

    Time: 17 ms (45.59%), Space: 21.6 MB (73.28%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int index=0;
+       int index=0;
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]!=0)
             {
-                swap(nums[i],nums[index]);
-                index++;
+                swap(nums[index],nums[i]);
+                    index++;
             }
         }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // int index=0;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]!=0)
+        //     {
+        //         swap(nums[i],nums[index]);
+        //         index++;
+        //     }
+        // }
         
     }
 };
