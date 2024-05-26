e67ba541fd99c1260bd49356d8ae980158334733
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2024-05-26 10:50:09 +0530

    Time: 26 ms (9.10%), Space: 27.8 MB (12.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -14,28 +14,6 @@ public:
             }
         }
         return maxElement;
-        
-        
-//         unordered_map<int ,int> m;
-//         int n=nums.size();
-//         for(int i=0;i<n;i++)
-//         {
-            
-//            m[nums[i]]++; 
-//             // auto it = m.find(nums[i]);
-//             // if(it != m.end() && it->second > n/2) return nums[i];
-//         }
-//         int maxElem,maxCount=0;
-//         for(auto it:m)
-//         {
-//             if(it.second > maxCount)
-//             {
-//                 maxCount = it.second;
-//                 maxElem = it.first;
-//             }
-//         }
-            
-        
-//         return maxElem;
+
     }
 };
