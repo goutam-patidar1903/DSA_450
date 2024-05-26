a0b6020f7700be4f80e764c649d7584e849e0dc1
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2024-05-26 23:21:39 +0530

    Time: 2 ms (47.22%), Space: 8.7 MB (21.58%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -18,22 +18,5 @@ public:
                 q.pop();
             }
         }
-        // for(int i=0;i<n-1;i++)
-        // {
-        //     for(int j=i+1;j<n;j++)
-        //     {
-        //         swap(matrix[i][j] , matrix[j][i]);
-        //     }
-        // }
-        // for(int i=0; i<n ; i++)
-        // {
-        //     int p=0,q=n-1;
-        //     while(p<q)
-        //     {
-        //         swap(matrix[i][p] , matrix[i][q]);
-        //         p++;
-        //         q--;
-        //     }
-        // }
     }
 };
