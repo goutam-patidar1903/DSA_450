c2baf4313214da0ff17687e8d4a5a88a6c68f748
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2024-05-26 10:47:34 +0530

    Time: 24 ms (16.05%), Space: 27.9 MB (12.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-    unordered_map<int,int> mpp;
-        int maxElement=0,maxCount=0;
-        for(int i=0;i<nums.size();i++)
+        //  unordered_map<int,int> mpp;
+        // int maxElement=0,maxCount=0;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     mpp[nums[i]]++;
+        //     if(mpp[nums[i]] > maxCount) 
+        //     {
+        //         maxElement=nums[i];   
+        //         maxCount=mpp[nums[i]];
+        //     }
+        // }
+        // return maxElement;
+        
+        
+        unordered_map<int ,int> m;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
         {
-            mpp[nums[i]]++;
-            if(mpp[nums[i]] > maxCount) 
+            
+           m[nums[i]]++; 
+            // auto it = m.find(nums[i]);
+            // if(it != m.end() && it->second > n/2) return nums[i];
+        }
+        int maxElem,maxCount=0;
+        for(auto it:m)
+        {
+            if(it.second > maxCount)
             {
-                maxElement=nums[i];   
-                maxCount=mpp[nums[i]];
+                maxCount = it.second;
+                maxElem = it.first;
             }
         }
-        return maxElement;
-        
-        
-//         unordered_map<int ,int> m;
-//         int n=nums.size();
-//         for(int i=0;i<n;i++)
-//         {
-            
-//            m[nums[i]]++; 
-//             // auto it = m.find(nums[i]);
-//             // if(it != m.end() && it->second > n/2) return nums[i];
-//         }
-//         int maxElem,maxCount=0;
-//         for(auto it:m)
-//         {
-//             if(it.second > maxCount)
-//             {
-//                 maxCount = it.second;
-//                 maxElem = it.first;
-//             }
-//         }
             
         
-//         return maxElem;
+        return maxElem;
     }
 };
