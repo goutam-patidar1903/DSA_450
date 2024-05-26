d6fbf65247fcdb3fee13ec9aa7d389b9912bddd0
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2024-05-26 10:44:45 +0530

    Time: 36 ms (5.98%), Space: 28 MB (12.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,26 +1,55 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int ,int> m;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
+    unordered_map<int,int> mpp;
+        int maxElement=0;
+        for(int i=0;i<nums.size();i++)
         {
+            mpp[nums[i]]++;
+            if(mpp[nums[i]] > (nums.size())/2) maxElement=nums[i];
             
-           m[nums[i]]++; 
-            // auto it = m.find(nums[i]);
-            // if(it != m.end() && it->second > n/2) return nums[i];
         }
-        int maxElem,maxCount=0;
-        for(auto it:m)
-        {
-            if(it.second > maxCount)
-            {
-                maxCount = it.second;
-                maxElem = it.first;
-            }
-        }
+        return maxElement;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //         unordered_map<int ,int> m;
+//         int n=nums.size();
+//         for(int i=0;i<n;i++)
+//         {
+            
+//            m[nums[i]]++; 
+//             // auto it = m.find(nums[i]);
+//             // if(it != m.end() && it->second > n/2) return nums[i];
+//         }
+//         int maxElem,maxCount=0;
+//         for(auto it:m)
+//         {
+//             if(it.second > maxCount)
+//             {
+//                 maxCount = it.second;
+//                 maxElem = it.first;
+//             }
+//         }
             
         
-        return maxElem;
+//         return maxElem;
     }
 };
