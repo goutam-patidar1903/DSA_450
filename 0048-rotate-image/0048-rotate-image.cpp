7ba8643688c579de965c19b26250a98282a49497
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2024-05-26 23:18:39 +0530

    Time: 2 ms (47.22%), Space: 8.8 MB (21.58%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,38 +2,38 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        // queue<int> q;
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=0;j<n;j++)
-        //     {
-        //         q.push(matrix[i][j]);       
-        //     }
-        // }
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=0;j<n;j++)
-        //     {
-        //         matrix[j][n-i-1]=q.front();
-        //         q.pop();
-        //     }
-        // }
-        for(int i=0;i<n-1;i++)
+        queue<int> q;
+        for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j=0;j<n;j++)
             {
-                swap(matrix[i][j] , matrix[j][i]);
+                q.push(matrix[i][j]);       
             }
         }
-        for(int i=0; i<n ; i++)
+        for(int i=0;i<n;i++)
         {
-            int p=0,q=n-1;
-            while(p<q)
+            for(int j=0;j<n;j++)
             {
-                swap(matrix[i][p] , matrix[i][q]);
-                p++;
-                q--;
+                matrix[j][n-i-1]=q.front();
+                q.pop();
             }
         }
+        // for(int i=0;i<n-1;i++)
+        // {
+        //     for(int j=i+1;j<n;j++)
+        //     {
+        //         swap(matrix[i][j] , matrix[j][i]);
+        //     }
+        // }
+        // for(int i=0; i<n ; i++)
+        // {
+        //     int p=0,q=n-1;
+        //     while(p<q)
+        //     {
+        //         swap(matrix[i][p] , matrix[i][q]);
+        //         p++;
+        //         q--;
+        //     }
+        // }
     }
 };
