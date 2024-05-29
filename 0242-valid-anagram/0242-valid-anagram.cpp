c9032d5208bf6a27720b11580ccc6288eda320eb
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2024-05-29 10:49:19 +0530

    Time: 6 ms (80.29%), Space: 9.7 MB (19.38%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> mpp;
+        for(int i=0;i<s.size();i++)
+        {
+            mpp[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            if(mpp[t[i]] == 0) return false;
+            mpp[t[i]]--;
+        }
+        for(auto it:mpp)
+        {
+            if(it.second != 0) return false;
+        }
+        return true;
+    }
+};
