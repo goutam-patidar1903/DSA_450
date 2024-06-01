ed079fe38413077a16ad01ef81f1ae336639ec62
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2024-06-01 10:04:02 +0530

    Time: 11 ms (43.10%), Space: 10.5 MB (92.19%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,13 +9,24 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == NULL || head->next == NULL) return false;
-        set<ListNode *> st;
-        while(head !=NULL)
+        // if(head == NULL || head->next == NULL) return false;
+        // set<ListNode *> st;
+        // while(head !=NULL)
+        // {
+        //     if(st.find(head) != st.end()) return true;
+        //     st.insert(head);
+        //     head=head->next;   
+        // }
+        // return false;
+        
+        //Tortoise Hare Method
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast!=NULL && fast->next!=NULL)
         {
-            if(st.find(head) != st.end()) return true;
-            st.insert(head);
-            head=head->next;   
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast) return true;
         }
         return false;
     }
