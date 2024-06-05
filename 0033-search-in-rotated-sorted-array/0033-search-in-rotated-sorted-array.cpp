bb99bf0f1ba3b6e906e30984cc5ad853c498e95c
Goutam Patidar <96475132+pgoutam1804@users.noreply.github.com>
2024-06-05 12:58:23 +0530

    Time: 0 ms (100.00%), Space: 13.5 MB (77.49%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -8,12 +8,12 @@ public:
             if(nums[mid]==target) return mid;
             if(nums[low]<=nums[mid])
             {
-                if(nums[low]<=target && target<=nums[mid]) high=mid-1;
+                if(nums[low]<=target && target<nums[mid]) high=mid-1;
                 else low=mid+1;
             }
             else
             {
-                if(nums[mid]<=target && target<=nums[high]) low=mid+1;
+                if(nums[mid]<target && target<=nums[high]) low=mid+1;
                 else high=mid-1;
             }
         }
