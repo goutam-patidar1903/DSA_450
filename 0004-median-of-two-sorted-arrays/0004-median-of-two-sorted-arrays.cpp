3d8b03e0b4b4fe5cecae367b262c9fa1b8d65582
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-06-08 10:26:03 +0530

    Time: 30 ms (33.64%), Space: 94.4 MB (90.16%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,36 +1,67 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        int size1=nums1.size();
-        int size2=nums2.size();
-        int i=0 , j=0;
-        while(i < size1 && j < size2)
-        {
-            if(nums1[i] < nums2[j]){
-                ans.push_back(nums1[i]);
-                i++;
-            }  
-            else{
-               ans.push_back(nums2[j]);
-                j++; 
-            }
-        }
-        while(i<size1)
-        {
-           ans.push_back(nums1[i]);
-                i++; 
-        }
-        while(j<size2)
-        {
-             ans.push_back(nums2[j]);
-                j++;
-        }
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+//         vector<int> ans;
+//         int size1=nums1.size();
+//         int size2=nums2.size();
+//         int i=0 , j=0;
+//         while(i < size1 && j < size2)
+//         {
+//             if(nums1[i] < nums2[j]){
+//                 ans.push_back(nums1[i]);
+//                 i++;
+//             }  
+//             else{
+//                ans.push_back(nums2[j]);
+//                 j++; 
+//             }
+//         }
+//         while(i<size1)
+//         {
+//            ans.push_back(nums1[i]);
+//                 i++; 
+//         }
+//         while(j<size2)
+//         {
+//              ans.push_back(nums2[j]);
+//                 j++;
+//         }
         
-       int n = size1 + size2;
-       if (n % 2 == 1) return (double)ans[n / 2];
+//        int n = size1 + size2;
+//        if (n % 2 == 1) return (double)ans[n / 2];
 
-       return ((double)ans[n / 2] + (double)ans[(n / 2) - 1]) / 2.0;
+//        return ((double)ans[n / 2] + (double)ans[(n / 2) - 1]) / 2.0;
+        
+        int n1 = a.size(), n2 = b.size();
+    //if n1 is bigger swap the arrays:
+    if (n1 > n2) return findMedianSortedArrays(b, a);
+
+    int n = n1 + n2; //total length
+    int left = (n1 + n2 + 1) / 2; //length of left half
+    //apply binary search:
+    int low = 0, high = n1;
+    while (low <= high) {
+        int mid1 = (low + high) >> 1;
+        int mid2 = left - mid1;
+        //calculate l1, l2, r1 and r2;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        if (mid1 < n1) r1 = a[mid1];
+        if (mid2 < n2) r2 = b[mid2];
+        if (mid1 - 1 >= 0) l1 = a[mid1 - 1];
+        if (mid2 - 1 >= 0) l2 = b[mid2 - 1];
+
+        if (l1 <= r2 && l2 <= r1) {
+            if (n % 2 == 1) return max(l1, l2);
+            else return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+        }
+
+        //eliminate the halves:
+        else if (l1 > r2) high = mid1 - 1;
+        else low = mid1 + 1;
+    }
+    return 0; //dummy statement
+
         
     }
 };
