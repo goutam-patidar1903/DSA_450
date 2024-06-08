f10a5f32413cfac88c5892d361e8269df4d73613
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-06-08 09:36:44 +0530

    Time: 4 ms (42.72%), Space: 9.1 MB (34.43%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    bool isPossible(vector<int>& nums , int s , int k)
+    {
+        int sum=0;
+        int count=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if((sum+nums[i]) <= s)
+            {
+                sum+=nums[i];
+            }
+            else
+            {
+                count++;
+                sum=nums[i];
+            }
+        }
+        return count>k;
+    }
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int low = *max_element(nums.begin(),nums.end());
+        int high = accumulate(nums.begin(),nums.end(),0);
+        // int ans=0;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(isPossible(nums , mid , k)) 
+            {
+                // ans=mid;
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+        return low;
+    }
+};
