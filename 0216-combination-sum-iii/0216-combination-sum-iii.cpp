66fd39bcd4aaa43da2f4edb176821211a8798680
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-06-17 09:31:32 +0530

    Time: 3 ms (49.55%), Space: 8 MB (58.19%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+    int sum = 0;
+    void dfs(vector<int>& cur, int k, int n, int idx, vector<vector<int>>& ans)
+    {
+        if (cur.size() == k && n==0)
+        {
+            ans.push_back(cur);
+            return;
+        }
+        else if (cur.size() == k && n>0) return;
+        for (int i = idx; i <= 9; i++)
+        {
+            cur.push_back(i);
+            dfs(cur, k, n-i, i + 1, ans);
+            cur.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> cur;
+        dfs(cur, k, n, 1, ans);
+        return ans;
+    }
+};
