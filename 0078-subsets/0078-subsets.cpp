5dde61b54ab8186f53fac7adf4400e820ec5a9fb
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-06-18 18:34:31 +0530

    Time: 4 ms (37.82%), Space: 8.2 MB (93.08%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,23 @@
 class Solution {
-    
+    void generateSubsets(int index, vector<int>& temp, vector<int>& nums, vector<vector<int>>& ans)
+    {
+        if(index >= nums.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[index]);
+        generateSubsets(index+1, temp, nums, ans);
+        temp.erase(find(temp.begin(),temp.end(),nums[index]));
+        generateSubsets(index+1, temp, nums, ans);
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-       vector<vector<int>> result;
-        int n = nums.size();
-        for (int i = 0; i < (1 << n); i++) {
-            vector<int> subset;
-            for (int j = 0; j < n; j++) {
-                if ((i & (1 << j)) ) {
-                    subset.push_back(nums[j]);
-                }
-            }
-            result.push_back(subset);
-        }
-        return result;
+        vector<vector<int>> ans;
+        vector<int> temp;
+        
+        generateSubsets(0, temp, nums, ans);
+        
+        return ans;
     }
 };
