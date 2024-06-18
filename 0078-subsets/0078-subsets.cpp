df61433964135174374ff8248ec5b6ff5e544ec3
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-06-18 18:33:29 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (77.92%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -7,7 +7,7 @@ public:
         for (int i = 0; i < (1 << n); i++) {
             vector<int> subset;
             for (int j = 0; j < n; j++) {
-                if ((i & (1 << j)) > 0) {
+                if ((i & (1 << j)) ) {
                     subset.push_back(nums[j]);
                 }
             }
