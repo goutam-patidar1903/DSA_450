76d35503d514ab4853fb72c3707435bb811a14de
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-06-19 10:21:32 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (21.34%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,18 +1,44 @@
 class Solution {
+    long double power(long double a ,long long b) {
+
+long double res = 1;
+if(b < 0) 
+b = b * -1;
+
+ while(b > 0){
+if(b&1) {
+    res = res * a;
+}
+a = a * a;
+b = b >> 1;
+ }
+
+return res;
+}
 public:
     double myPow(double x, int n) {
         
-        if(n==0) return 1;
-        if(n<0) {
-            n = abs(n);
-            x = 1/x;
+        // if(n==0) return 1;
+        // if(n<0) {
+        //     n = abs(n);
+        //     x = 1/x;
+        // }
+        // if(n%2==0){
+        //     return myPow(x*x, n/2);
+        // }
+        // else{
+        //     return x*myPow(x, n-1);
+        // }
+        
+         long double ans = power(x,n);
+        long double a = 1;
+
+        if(n < 0) {
+            double sol = a / ans;
+
+            return sol;
         }
-        if(n%2==0){
-            return myPow(x*x, n/2);
-        }
-        else{
-            return x*myPow(x, n-1);
-        }
+        return ans;
         
     }
 };
