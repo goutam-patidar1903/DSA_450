2367f9fb838fdcf7bf0446f3a681608066849fba
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-06-24 11:37:48 +0530

    Time: 11 ms (59.61%), Space: 10.2 MB (36.53%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -17,28 +17,31 @@ public:
             k--;
         }
         
-        // Construct the resulting string from the stack
-        std::string result;
-        while (!stack.empty()) {
-            result += stack.top();
+        // // Construct the resulting string from the stack
+        // std::string result;
+        // while (!stack.empty()) {
+        //     result += stack.top();
+        //     stack.pop();
+        // }
+        // std::reverse(result.begin(), result.end()); // Reverse to get the correct order
+        
+//         // Remove leading zeros
+//         size_t pos = result.find_first_not_of('0');
+//         result = (pos == std::string::npos) ? "0" : result.substr(pos);
+        
+//         return result;
+        
+       string ret="";
+        while(!stack.empty()){
+            ret+=stack.top();
             stack.pop();
         }
-        std::reverse(result.begin(), result.end()); // Reverse to get the correct order
+        reverse(ret.begin(),ret.end());
+        int a=0;
+        while(ret[a]=='0' && a<ret.size()){
+            a++;
+        }
         
-        // Remove leading zeros
-        size_t pos = result.find_first_not_of('0');
-        result = (pos == std::string::npos) ? "0" : result.substr(pos);
-        
-        return result;
-        
-//         string ans;
-//         while(!st.empty()){
-//         ans=(char)st.top()+ans; 
-//         st.pop();
-//         }
-//         int pos=0;
-//         while(pos<ans.length() && ans[pos]=='0') pos++;
-        
-//         return (pos==ans.length())? "0" : ans.substr(pos);
+        return (a==ret.size())? "0" : ret.substr(a);
     }
 };
