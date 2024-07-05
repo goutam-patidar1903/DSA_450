3b1c3c848c31b82e6f409d5e9945ad9c34c0fba4
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-07-05 09:56:08 +0530

    Time: 8 ms (71.31%), Space: 21.7 MB (12.93%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -10,34 +10,23 @@
  * };
  */
 class Solution {
-    int getHeight(TreeNode* root){
+    int dfsHeight(TreeNode* root){
         if(!root) return 0;
         
-        int lh=getHeight(root->left);
-        int rh=getHeight(root->right);
+        int lh=dfsHeight(root->left);
+        if(lh==-1) return -1;
+        
+        int rh=dfsHeight(root->right);
+        if(rh==-1) return -1;
+        
+        if(abs(lh-rh)>1) return -1;
         
         return 1+ max(lh,rh);
     }
 public:
     bool isBalanced(TreeNode* root) {
-        
-        if (root == nullptr) {
-            return true;
-        }
-
-        // Calculate the height of left and right subtrees
-        int leftHeight = getHeight(root->left);
-        int rightHeight = getHeight(root->right);
-
-        // Check if the absolute difference in heights
-        // of left and right subtrees is <= 1
-        if (abs(leftHeight - rightHeight) <= 1 &&
-            isBalanced(root->left) &&
-            isBalanced(root->right)) {
-            return true;
-        }
-
-        // If any condition fails, the tree is unbalanced
-        return false;
+       
+        return dfsHeight(root) != -1;
+       
     }
 };
