adf0b132b0791971bceea98b8068d12cca314247
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-10-11 21:45:17 +0530

    Time: 84 ms (90.24%), Space: 70.2 MB (88.47%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,28 +1,46 @@
 class Solution {
+    int atMostCnt(vector<int>& nums, int k) {
+        if(k < 0) return 0;
+        int l = 0, r= 0, ans = 0;
+        long long sum = 0;
+        int n = nums.size();
+        for (r= 0; r< n; r++) {
+            sum += nums[r]%2;
+            while (sum > k) {
+                sum -= nums[l++]%2;
+            }
+            ans += r - l + 1;
+        }
+       
+        return ans;
+    }
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int l=0;
-        int ans=0;
-        int ki=0;
-        int cnt=0;
-        for(int r=0;r<nums.size();r++)
-        {
-            if(nums[r]%2 == 1) {
-                k--;
-                cnt=0;
-            }
+        
+        return atMostCnt(nums,k) - atMostCnt(nums,k-1);
+        
+//         int l=0;
+//         int ans=0;
+//         int ki=0;
+//         int cnt=0;
+//         for(int r=0;r<nums.size();r++)
+//         {
+//             if(nums[r]%2 == 1) {
+//                 k--;
+//                 cnt=0;
+//             }
             
-            while(k==0)
-            {
+//             while(k==0)
+//             {
                 
-                if(nums[l]%2 == 1) k++;
-                cnt++;
-                l++;
-            }
-            ans+=cnt;
+//                 if(nums[l]%2 == 1) k++;
+//                 cnt++;
+//                 l++;
+//             }
+//             ans+=cnt;
 
-        }
-        return ans;
+//         }
+//         return ans;
         
         
 //         int ans=0;
