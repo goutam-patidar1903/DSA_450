08770e7adba32128b1d360403a90099fc4f0ba3e
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-10-11 21:40:23 +0530

    Time: 91 ms (72.29%), Space: 70.3 MB (72.23%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -23,5 +23,18 @@ public:
 
         }
         return ans;
+        
+        
+//         int ans=0;
+//         for(int i=0;i<nums.size();i++) {
+//             int oddCnt=0;
+//             for(int j=i;j<nums.size();j++) {
+//                 if(nums[j] % 2 == 1) oddCnt ++;
+//                 if(oddCnt == k) ans++;
+//                 if(oddCnt > k) break;
+//             }
+            
+//         }
+//         return ans;
     }
 };
