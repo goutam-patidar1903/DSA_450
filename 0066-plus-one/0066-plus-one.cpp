155a1bb0603c572edc0671583c875541bd89788c
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-11-05 20:33:17 +0530

    Time: 0 ms (100.00%), Space: 10.6 MB (47.99%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-       // int n=digits.size();
-       //  for(int i=n-1;i>=0;i--){
-       //      if(digits[i]<9){
-       //          digits[i]+=1;
-       //          return digits;
-       //      }
-       //      else {
-       //          digits[i]=0;
-       //          if(i==0) digits.insert(digits.begin()+0,1);
-       //      }
-       //  }
-       //  return digits;
-        vector<int> ans;
-
-        int n = digits.size();
-        int carry = 1;
-        for(int i=n-1; i>=0; i--){
-            int a = digits[i];
-            int b = carry;
-            int sum = a+b;
-            int r = sum%10;
-            carry = sum/10;
-            ans.insert(ans.begin(), r);
+       int n=digits.size();
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]+=1;
+                return digits;
+            }
+            else {
+                digits[i]=0;
+                if(i==0) digits.insert(digits.begin()+0,1);
+            }
         }
-        if (carry>0){
-            ans.insert(ans.begin(), carry);
-        }
+        return digits;
+//         vector<int> ans;
 
-        return ans;
+//         int n = digits.size();
+//         int carry = 1;
+//         for(int i=n-1; i>=0; i--){
+//             int a = digits[i];
+//             int b = carry;
+//             int sum = a+b;
+//             int r = sum%10;
+//             carry = sum/10;
+//             ans.insert(ans.begin(), r);
+//         }
+//         if (carry>0){
+//             ans.insert(ans.begin(), carry);
+//         }
+
+//         return ans;
     }
 };
