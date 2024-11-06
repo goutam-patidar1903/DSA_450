4b35a98126c300df2f47a7eea2952ec96af6fb86
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-11-06 19:37:31 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (13.17%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -5,12 +5,10 @@ public:
 
         while (columnNumber) {
             columnNumber--;
-            // Get the last character and append it at the end of string.
             ans = ans + (char)((columnNumber) % 26 + 'A');
             columnNumber = (columnNumber) / 26;
         }
 
-        // Reverse it, as we appended characters in reverse order.
         reverse(ans.begin(), ans.end());
         return ans;
     }
