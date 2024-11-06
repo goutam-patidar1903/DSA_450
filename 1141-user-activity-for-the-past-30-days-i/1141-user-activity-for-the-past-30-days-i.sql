bdd69f140ad299b66eb3d880c0453d00c237bca6
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-11-06 09:43:49 +0530

    Time: 573 ms (42.86%), Space: 0B (100.00%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -3,8 +3,19 @@
 SELECT 
     activity_date as day , 
     COUNT(DISTINCT user_id) as active_users
-FROM Activity
+FROM 
+    Activity
 WHERE 
-    activity_date >= "2019-06-28" AND
-    activity_date <= "2019-07-27"
-GROUP BY activity_date;
+    activity_date BETWEEN DATE_SUB("2019-07-27", INTERVAL 29 DAY) AND "2019-07-27"
+GROUP BY 
+    activity_date;
+
+
+# SELECT 
+#     activity_date as day , 
+#     COUNT(DISTINCT user_id) as active_users
+# FROM Activity
+# WHERE 
+#     activity_date >= "2019-06-28" AND
+#     activity_date <= "2019-07-27"
+# GROUP BY activity_date;
