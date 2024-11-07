e78a08dc33fbf0532b21920fa5d1efc1a21431ce
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-11-07 16:39:05 +0530

    Time: 4 ms (27.68%), Space: 14.2 MB (13.12%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        
+        // my code 
+//         vector<int> ans;
+//         unordered_map<int,int> m;
+        
+//         for(int i=0;i<nums1.size();i++) {
+//             int n = nums1[i];
+
+//             if(!m[n]) {
+//                 m[n]++;
+//                 for(int j=0;j<nums2.size();j++){
+//                     if(n==nums2[j]){
+//                         ans.push_back(n);
+//                         j=nums2.size();
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return ans;
+        
+        
+        // optimized code 
+        vector<int > result;
+    unordered_set<int> set_nums2(nums2.begin(), nums2.end()); 
+    unordered_set<int> result_set;
+
+    for (int n : nums1) {
+        if (set_nums2.count(n)) {
+            result_set.insert(n); 
+        }
+    }
+
+    // Convert result_set to a vector
+    return std::vector<int>(result_set.begin(), result_set.end());
+        
+        
+    }
+};
