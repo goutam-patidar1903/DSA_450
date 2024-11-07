5c51acc21b8f1ef2287a42784c61fc7c941023de
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-11-07 16:55:57 +0530

    Time: 0 ms (100.00%), Space: 14.7 MB (8.47%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        
         unordered_map<int,int> mpp;
-        
-        for (int n : nums2) {
-        mpp[n]++;
-    }
-        
         vector<int> result;
-        for(int n : nums1){
+
+        if (nums1.size() > nums2.size()) {
+            swap(nums1, nums2);
+        }
+        
+        for (int n : nums1) {
+            mpp[n]++;
+        }
+        
+        for(int n : nums2){
             if(mpp[n]){
                 result.push_back(n);
                 mpp[n]--;
             }
         }
-return result;
+        return result;
     }
 };
