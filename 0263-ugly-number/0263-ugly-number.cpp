5a690911d352c683af71b5f21cb2b02aa7ba20ea
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-11-08 17:34:34 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (14.13%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     bool isUgly(int n) {
         while(1){
-            cout<<n<<endl;
             while(n%2==0 && n!=0) n=n/2;
-            cout<<n<<endl;
             while(n%3==0 && n!=0) n=n/3;
             while(n%5==0 && n!=0) n=n/5;
             break;
         }
-        cout<<n;
         return  n==1;
     }
 };
