be61647c4d1ed02ed310579b6ecb41e9b2cf3af3
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-12-21 19:58:25 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (24.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -15,6 +15,14 @@ class Solution {
         int N = nums.length;
         int dp[] = new int [N+1];
         Arrays.fill(dp,-1);
-        return solveUtil(N-1, nums, dp);
+        // return solveUtil(N-1, nums, dp);
+        dp[0] = nums[0];
+        for(int i=1;i<N;i++){
+           int pick = nums[i];
+           if (i > 1) pick += dp[i - 2];
+           int nonPick = dp[i - 1]; 
+            dp[i] = Math.max(pick, nonPick);
+        }
+        return dp[N-1];
     }
 }
