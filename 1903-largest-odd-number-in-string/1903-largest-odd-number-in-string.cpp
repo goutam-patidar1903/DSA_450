fd8f59d01b5d4162e03afacb92a157b641e6e28e
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-12-23 21:35:28 +0530

    Time: 0 ms (100.00%), Space: 17.7 MB (34.42%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -6,7 +6,7 @@ public:
 
         for(i=n-1; i>=0; i--){
             if(num[i]%2!=0) break;
-            else if(i==0 and num[i]%2==0)  return "";
+            // else if(i==0 and num[i]%2==0)  return "";
         }
         return num.substr(0,i+1);
     }
