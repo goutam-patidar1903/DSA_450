91b5ae141dac22b86dee1510b3164793cd279bcf
Goutam Patidar <96475132+goutam-patidar1903@users.noreply.github.com>
2024-12-23 21:55:18 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (64.87%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -15,8 +15,19 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> temp;
+        int n = nums.size();
         
-        generateSubsets(0, temp, nums, ans);
+        // generateSubsets(0, temp, nums, ans);
+        
+        for(int i=0;i<(1<<n);i++){
+            vector<int> tt;
+            
+            for(int j=0;j<n;j++){
+                if((i & (1<<j)) > 0) tt.push_back(nums[j]);
+            }
+            
+            ans.push_back(tt);
+        }
         
         return ans;
     }
